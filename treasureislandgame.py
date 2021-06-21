34c4e26818a57c67b2c11507051efcf5b6024c05
Aldred <82555959+coolaldred@users.noreply.github.com>
2021-06-21 17:45:47 +0800

    Create treasureislandgame.py

---
+++ treasureislandgame.py
@@ -0,0 +1,50 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+direction = input("You\'re at the crossroad. Which way you want to go? \"Left\" or \"Right\"?:").lower()
+# print (direction)
+
+if direction == 'right':
+  print("Fall into a rabbit hole. Game Over")
+else:
+  print("You reach the lake.")
+  action = input("You need to reach destination across the lake. What will you do? Swim or Wait ").lower()
+  if action == 'swim':
+    print("Attacked by the trout. Game over")
+  else:
+    print("The boat arrived at the dock and took you to the treasure house.")
+    
+    doorcolors = input("There are 4 doors. Red, Yellow, Blue & Brown. Which the door you will enter? ").lower()
+    if doorcolors == 'red':
+      print("You burned by fire. Game over")
+    elif doorcolors == 'yellow':
+      print("You found the treasure!")
+    elif doorcolors == 'blue':
+      print("You are eaten by the beast. Game over. Beast: Burp..")
+    else:
+      print("GAME OVER!")
+
+
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
